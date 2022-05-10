e3e18cf535bd5b246544515e2a612b512ff52d5c
Mike5801 <A01610836@itesm.mx>
2022-05-10 12:36:09 -0500

    Change comments to spanish

--- sumNumbers.py
+++ sumNumbers.py
@@ -1,9 +1,9 @@
-# Store input numbers
+# Entrada de los numeros
 num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
 
-# Add two numbers
+# Agrega dos numeros
 sum = float(num1) + float(num2)
 
-# Display the sum
+# Impresion en pantalla de los numeros sumados
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
